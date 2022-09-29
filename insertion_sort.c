140d5e7a74703c6956651b3adef8bc9de5c6bce6
[Andre Mueller] <[andrexmueller@yahoo.com.br]>
2022-09-28 21:26:18 -0300

    insertion sort

---
+++ insertion_sort.c
@@ -0,0 +1,51 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<stdbool.h>
+
+void print_arr(int arr[], int length) {
+    printf("[ ");
+    for (int i = 0; i < length; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("]\n");
+}
+
+void sort(int arr[], int length) {
+    bool sorted = false;
+    int tmp;
+    while (!sorted) {
+        sorted = true;
+        for (int i = 1; i < length; i++) {
+            if (arr[i-1] > arr[i]) {
+                tmp = arr[i-1];
+                arr[i-1] = arr[i];
+                arr[i] = tmp;
+                sorted = false;
+            }
+        }
+    }
+    return;
+}
+
+void random_arr(int arr[], int n) {
+    time_t t;
+    srand((unsigned) time(&t));
+
+    for (int i = 0; i < n; i++) {
+        arr[i]  = rand() % 50;   
+    }
+    return;
+}
+
+
+int main(int argc, char *argv[]) {
+    
+    int arr[10];
+    random_arr(arr, 10);
+    print_arr(arr, 10);
+    sort(arr, 10);
+    print_arr(arr, 10);
+    
+
+
+}
